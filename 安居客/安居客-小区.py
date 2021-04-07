693881d4b9f01abc3ee7242837f8ae2762daa40a
shenyumao <shenyumao@189auto.com.cn>
2021-04-07 15:57:27 +0800

    1

--- 安居客/安居客-小区.py
+++ 安居客/安居客-小区.py
@@ -157,8 +157,8 @@ def get_parseInfo(city,url,area_name):
     for house in house_div:
         item = {}
         item['city_name'] = city
-        item['标题'] = house.xpath('string(./div[@class="li-info"]/div[@class="li-title"]/a)')
-        item['详情url'] = house.xpath('string(./div[@class="li-info"]/div[@class="li-title"]/a/@href)')
+        item['标题'] = house.xpath('string(./div[@class="li-info"]/div[@class="li-title"]/div[@class="nowrap-min li-community-title"])')
+        item['详情url'] = house.xpath('string(@href)')
         check_map = house.xpath('string(./div[@class="li-info"]/div[@class="detail nowrap-max"]/span[3]/a/@href)')
         try:
             item['latitude'] = re.search('l1=(.*?)&',check_map).group(1)
@@ -167,10 +167,10 @@ def get_parseInfo(city,url,area_name):
             item['latitude'] = ''
             item['longitude'] = check_map
         try:
-            item['竣工时间'] = re.search('\d+',house.xpath('string(./div[@class="li-info"]/p[@class="props nowrap"]/span[@class="year"])')).group()
+            item['竣工时间'] = re.search('\d+', house.xpath('string(./div[@class="li-info"]/div[@class="props nowrap"]/span[@class="year"])')).group()
         except:
             item['竣工时间'] = house.xpath('string(./div[@class="li-info"]/p[@class="props nowrap"]/span[@class="year"])')
-        item['地址'] = house.xpath('string(./div[@class="li-info"]/p[@class="props nowrap"]/span[last()])').replace(' - ','-')
+        item['地址'] = house.xpath('string(./div[@class="li-info"]/div[@class="props nowrap"]/span["year"]/following-sibling::span/following-sibling::span)').replace(' - ', '-')
         try:
             item['二手房上架数'] = re.search('\d+',house.xpath('string(./div[@class="li-info"]/div[@class="detail nowrap-max"]/span[1])')).group()
         except:
@@ -179,7 +179,7 @@ def get_parseInfo(city,url,area_name):
             item['在租套数'] = re.search('\d+',house.xpath('string(./div[@class="li-info"]/div[@class="detail nowrap-max"]/span[2])')).group()
         except:
             item['在租套数'] = house.xpath('string(./div[@class="li-info"]/div[@class="detail nowrap-max"]/span[2])')
-        item['价格'] = house.xpath('string(./div[@class="li-side"]/p/strong)')
+        item['价格'] = house.xpath('string(./div[@class="li-side"]/div[@class="community-price"]/strong)')
         perpare = house.xpath('string(./div[@class="li-side"]/span)').strip().replace('\n','')
         sign = house.xpath('string(./div[@class="li-side"]/span/@class)')
         if 'red' in sign:
@@ -187,6 +187,7 @@ def get_parseInfo(city,url,area_name):
         elif 'green' in sign:
             item['涨跌幅'] = '-' + perpare
         print(item)
+        item['抓取时间'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         info_base.insert_one(item)
     has_spider.insert_one({'url':url,'city':area_name})
     time.sleep(5)
@@ -200,6 +201,7 @@ def get_parseInfo(city,url,area_name):
 
 
 if __name__ == '__main__':
+
     for item in city_map:
         key = item
         url = city_map[item]
@@ -213,8 +215,8 @@ if __name__ == '__main__':
             if url in has_spider_list:
                 continue
             get_parseInfo(key,url,area_name)
-        statis_output('{}_{}.csv'.format(key,time.strftime("%Y-%m", time.localtime())),
-
-                      ['city_name', '标题', '详情url','latitude','longitude', '竣工时间', '地址', '二手房上架数', '在租套数', '价格', '涨跌幅'],
-                      info_base,key)
+        # statis_output('{}_{}.csv'.format(key,time.strftime("%Y-%m", time.localtime())),
+        #
+        #               ['city_name', '标题', '详情url','latitude','longitude', '竣工时间', '地址', '二手房上架数', '在租套数', '价格', '涨跌幅'],
+        #               info_base,key)
         # break
