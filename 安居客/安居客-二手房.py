bcfda1aedb693da17ca08d0c6911268e4b116eb5
赵诣 <277879979@qq.com>
2021-04-07 13:14:58 +0800

    先去掉代理

--- 安居客/安居客-二手房.py
+++ 安居客/安居客-二手房.py
@@ -64,6 +64,7 @@ def get_html(url):
             try:
                 # response = requests.get(url, headers=headers,
                 #                         proxies={"https": "https://{}".format(proxy)}, timeout=(2, 5))
+                
                 response = requests.get(url, headers=headers, timeout=(2, 5))
                 response.encoding = 'utf-8'
                 html = etree.HTML(response.text)
