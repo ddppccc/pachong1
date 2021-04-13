36b884148a1c9acbcb6984cbaa4c9a2cc4b1c6d8
赵诣 <277879979@qq.com>
2021-04-13 10:06:10 +0800

    新增高德配置

---
+++ 高德POI/gaode_poi.py
@@ -0,0 +1,9 @@
+import config
+import requests
+from requests.adapters import HTTPAdapter
+
+s = requests.Session()
+s.mount('http://', HTTPAdapter(max_retries=3))#设置重试次数为3次
+s.mount('https://', HTTPAdapter(max_retries=3))
+
+
