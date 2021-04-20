c6126d682dc73ff27e444e5a6a02ddcd927277aa
陈吉义 <chenjiyi@baojinhui.com>
2021-04-20 15:07:24 +0800

    1

--- 土地市场网招拍挂/2_地块公示_详情.py
+++ 土地市场网招拍挂/2_地块公示_详情.py
@@ -12,7 +12,8 @@ import hashlib
 from queue import Queue
 from threading import Thread, Lock
 from fontTools.ttLib import TTFont
-from scrapy import Selector
+from lxml import etree
+# from scrapy import Selector
 from config import base_font
 
 from 验证码 import TuDi
@@ -47,35 +48,69 @@ def get_city_province(df, region, title):
                 city = d.iloc[i]['city']
                 province = d.iloc[i]['province']
     return city, province, region
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "Cookie": "global_cookie=f1v4y6cur4h7034ieaxs84qnk17jxobmy8b; Integrateactivity=notincludemc; newhouse_user_guid=50E5E180-78E4-4D10-DD8D-77E13BC96848; lastscanpage=0; budgetLayer=1%7Cbj%7C2019-07-30%2011%3A26%3A08; resourceDetail=1; new_search_uid=fd91130e51ddd1f2d8e5a7847545c3d8; __utmc=147393320; __utma=147393320.184954923.1564457033.1564642303.1564646708.12; __utmz=147393320.1564646708.12.5.utmcsr=search.fang.com|utmccn=(referral)|utmcmd=referral|utmcct=/captcha-verify/redirect; __utmt_t0=1; __utmt_t1=1; __utmt_t2=1; logGuid=cb4c3441-3467-4c19-ba09-1dd86b143529; g_sourcepage=ehlist; city=anshan; unique_cookie=U_11rfckz759men2uard7dzzy1i1ejys2c6di*36; __utmb=147393320.15.10.1564646708",
+    "Referer": "https://www.landchina.com/DesktopModule/BizframeExtendMdl/workList/bulWorkView.aspx",
+    "upgrade-insecure-requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+}
+def get_proxy():
+    try:
+            return requests.get('http://47.106.223.4:50002/get/').json().get('proxy')
+            # return '111.202.83.35:80'
+    except:
+        num = 3
+        while num:
+            try:
+                return requests.get('http://47.106.223.4:50002/get/').json().get('proxy')
+            except:
+                print('暂无ip，等待20秒')
+                time.sleep(20)
 
-
+                num -= 1
+        print('暂无ip')
+def get_html(url):
+    proxies = {"https": get_proxy()}
+    try:
+        response = requests.get(url, headers=headers,proxies=proxies, timeout=10)
+        encod = response.apparent_encoding
+        if encod.upper() in ['GB2312', 'WINDOWS-1254']:
+            encod = 'gbk'
+        response.encoding = encod
+        if '验证码' in response.text or 'verifyimg' in response.text:
+            print('出现验证码，更换ip')
+            return get_html(url)
+        return response
+    except Exception as e:
+        print('get_html错误', proxies,e)
+        return  get_html(url)
 # 获取详情页面
 def get_parse_url(url):
+    # return get_html(url)
+    proxy, cookie = TuDi().run()
+    # print(proxy,cookie)
+    try:
+        header = {
+            "Host": "www.landchina.com",
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0",
+            "Accept": "text/css,*/*;q=0.1",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip, deflate",
+            'Cookie': cookie,
+            "Referer": url,
+            "Connection": "keep-alive"
+        }
 
-
-    while True:
-        proxy, cookie = TuDi().run()
-        try:
-            header = {
-                "Host": "www.landchina.com",
-                "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0",
-                "Accept": "text/css,*/*;q=0.1",
-                "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-                "Accept-Encoding": "gzip, deflate",
-                'Cookie': cookie,
-                "Referer": url,
-                "Connection": "keep-alive"
-            }
-
-            response = requests.get(url, headers=header, timeout=(2, 10), proxies=proxy)
-            if '验证码' in response.text or 'verifyimg' in response.text:
-                continue
+        response = requests.get(url, headers=header, timeout=(10, 10), proxies=proxy)
+        if '验证码' in response.text or 'verifyimg' in response.text:
             response.encoding = 'gbk'
-            return response
-        except Exception as e:
-            print('详情页: ', e)
-            time.sleep(2)
-            continue
+        return response
+    except Exception as e:
+        print('获取详情页错误',url, e)
+        return get_parse_url(url)
 
 
 # 获取解析后的值
@@ -138,42 +173,61 @@ def get_font_woff(url):
     font = TTFont(fileName)
     return font
 
+def getInfo():
+    list = f.readline()
+    l = list.split(',')
+    info_url = l[-1].strip()
+    region = l[0]
+    date = l[2]
+    return info_url,region,date
+
 
 def parse(df_map):
     while True:
-        if queue.empty() == True:
-            break
-        df = queue.get()
-        info_url = "".join(df['标题url'].tolist())
-        print("info_url: ", info_url)
-        if 'land' not in info_url:
-            queue.task_done()
-            continue
+        # if queue.empty() == True:
+        #     break
+        # df = queue.get()
+        # info_url = "".join(df['标题url'].tolist())
+        # print("info_url: ", info_url)
+        # if 'land' not in info_url:
+        #     queue.task_done()
+        #     continue
 
-
+        # with open('土地数据/地块公示.csv', encoding='gbk', mode='r') as f:
+        #     list=f.readlines()
+        #     for i in list:
+        #         l=i.split(',')
+        #         info_url=l[-1].strip()
+        #         region=l[0]
+        #         date=l[2]
+        #         break
+        info_url, region, date = getInfo()
         res = get_parse_url(info_url)
         if not res or '没有相关说明信息' in res.text:
             with open('土地log/地块公示坏url.text', 'a') as fp:
                 fp.write(info_url)
                 fp.write('\n')
-            queue.task_done()
+            # queue.task_done()
             print('nodata')
             continue
         res.encoding = 'gbk'
 
+
+        # print('开始获取字体加密文件')
         # 获取字体加密文件
         try:
             font_url = re.findall("url\('\.\./\.\./\.\./(.*)'\) format\('woff'\),", res.text)[0]
         except:
-            queue.task_done()
+            # queue.task_done()
             print(res.text)
             print('#############')
             continue
 
+        # print('开始获取font')
         Font = get_font_woff(font_url)
         if not Font:
             print('没有FONT')
-            queue.task_done()
+            # queue.task_done()
             continue
 
         # response = Selector(text=res.text)
@@ -182,14 +236,17 @@ def parse(df_map):
         # table_lxml = Selector(text=table_decode)
 
         # 法二
-        response = decode_font(Font, res.text)
-        response = Selector(text=response)
+        # response = decode_font(Font, res.text)
+        # response = Selector(text=response)
+        response = etree.HTML(res.text)
         table_lxml = response.xpath('//*[@id="tdContent"]')
 
-        title_ = ''.join(response.xpath('//*[@id="tdContent"]/table/tbody/tr[1]/td/text()').extract())
-        title = title_.strip()
+        title = response.xpath('/html/body/form/table/tbody/tr[3]/td/table/tbody/tr[6]/td/table/tbody/tr/td/table/tbody/tr[1]/td/text()[1]')
 
-        tables = table_lxml.xpath(".//table[@width='100%']")
+        print('title',title)
+
+        tables = table_lxml[0].xpath(".//table[@width='100%']")
+        # print(tables)
         if not tables:
             with open('土地log/地块公示坏url.text', 'a') as fp:
                 fp.write(info_url)
@@ -197,119 +254,127 @@ def parse(df_map):
 
         for table in tables:
             item = {}
-            item['行政区'] = "".join(df['行政区'].tolist())
-            item['标题'] = response.xpath("//span[@id='lblTitle']/text()").get()
+            # item['行政区'] = "".join(df['行政区'].tolist())
+            item['行政区'] = region
+            item['标题'] = response.xpath("//span[@id='lblTitle']/text()")
             city, province, region = get_city_province(df_map, item['行政区'], item['标题'])
             item['省份'] = province
             item['城市'] = city
             item['行政区'] = region
 
-            item['公告编号'] = title
-            item['成交日期'] = "".join(df['公示日期'].tolist())
-            item['标题url'] = "".join(df['标题url'].tolist())
+            # item['公告编号'] = title
+            item['成交日期'] = date
+            item['标题url'] = info_url
 
-            item['地块编号'] = (table.xpath(".//td[contains(text(), '编号')]/following-sibling::td[1]/text()").get() or '').strip() or\
-                           (table.xpath(".//td[contains(text(), '地块编号')]/following-sibling::td[1]/text()").get() or '').strip()
-            try:
-                item['土地面积'] = table.xpath(
-                ".//td[contains(text(), '土地面积(公顷)')]/following-sibling::td[1]/text()").get().strip()
-            except:
-                item['土地面积'] = ''
-            item['地块位置'] = format_str(
-                    table.xpath(".//td[contains(text(), '置')]/following-sibling::td[1]/text()").get() or '')
-            item['出让年限'] = format_str(
-                    table.xpath(".//td[contains(text(), '出让年限')]/following-sibling::td[1]/text()").get() or '')
-            item['成交价(万元)'] = format_str(
-                    table.xpath(".//td[contains(text(), '成交价(万元)')]/following-sibling::td[1]/text()").get() or '')
-            item['土地用途'] = format_str(table.xpath(
-                ".//td[contains(text(), '土地用途')]/following-sibling::td[1]/text()").get() or '')
+            item['地块编号'] = (table.xpath(".//td[contains(text(), '编号')]/following-sibling::td[1]/text()") or '')[0].strip() or\
+                           (table.xpath(".//td[contains(text(), '地块编号')]/following-sibling::td[1]/text()") or '')[0].strip()
+            # print(item['地块编号'])
+            item['土地面积'] = table.xpath('.//tr[2]/td[2]/text()')[0] or ''
+            # item['地块位置'] = format_str(
+            #         table.xpath(".//td[contains(text(), '置')]/following-sibling::td[1]/text()") or '')
+            item['地块位置'] = table.xpath(".//td[contains(text(), '置')]/following-sibling::td[1]/text()")[0] or ''
 
-            a = table.xpath(".//tr/td[1][contains(text(), '用地')]/text()").extract()
-            b = table.xpath(".//tr/td[1][contains(text(), '用地')]/following-sibling::td[1]/text()").extract()
-            item['明细用途'] = dict(zip(a, b))
-            print(f"明细用途: {item['明细用途']} ")
+            # item['出让年限'] = format_str(
+            #         table.xpath('.//*[@id="tdContent"]/table/tbody/tr[2]/td/table/tbody/tr[2]/td[4]/text()') or '')
+            item['出让年限'] = table.xpath('.//tr[2]/td[4]/text()')[0] or ''
 
-            item['受让单位'] = format_str(table.xpath(
-                ".//td[contains(text(), '受让')]/following-sibling::td[1]/text()").get() or '')
-            item['土地使用权限'] = format_str(table.xpath(
-                ".//td[contains(text(), '土地使用条件：')]/following-sibling::td[1]/text()").get() or '')
-            item['备注'] = format_str(table.xpath( ".//td[contains(text(), '备')]/following-sibling::td[1]/text()").get() or '')
-            print(item)
+            # item['成交价(万元)'] = format_str(
+            #         table.xpath('.//*[@id="tdContent"]/table/tbody/tr[2]/td/table/tbody/tr[2]/td[6]/text()') or '')
+            item['成交价(万元)'] = table.xpath('.//tr[2]/td[6]/text()')[0] or ''
+
+            # item['土地用途'] = format_str(
+            #     table.xpath('.//*[@id="tdContent"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td[6]/text()') or '')
+            item['土地用途'] = table.xpath('.//tr[1]/td[6]/text()')[0] or ''
+
+            a = table.xpath(".//tr[4]/td[1]/text()")[0]
+            b = table.xpath(".//tr[5]/td[1]/text()")[0]
+            item['明细用途'] = a +'：'+ b
+            # print(f"明细用途: {item['明细用途']} ")
+
+            # item['受让单位'] = format_str(table.xpath(
+            #     ".//td[contains(text(), '受让')]/following-sibling::td[1]/text()") or '')
+            # item['土地使用权限'] = format_str(table.xpath(
+            #     ".//td[contains(text(), '土地使用条件：')]/following-sibling::td[1]/text()") or '')
+            item['备注'] = format_str(table.xpath( ".//td[contains(text(), '备')]/following-sibling::td[1]/text()")[0] or '')
+            print('item',item)
+        # break
 
 
-            # 保存数据
-            savePath = os.path.join('土地数据', '地块公示_详情.csv')
-            # 判断是否存在当前文件夹
-            if not os.path.exists(savePath):  # 不存在
-                with open(savePath, 'a', newline='', encoding='gbk') as csvfile:
-                    writer = csv.writer(csvfile)
-                    lock = Lock()
-                    lock.acquire()
-                    writer.writerow(list(item.keys()))
-                    writer.writerow(list(item.values()))
-                    lock.release()
-            else:
-                with open(savePath, 'a', newline='', encoding='gbk') as csvfile:
-                    writer = csv.writer(csvfile)
-                    lock = Lock()
-                    lock.acquire()
-                    writer.writerow(list(item.values()))
-                    lock.release()
-        print()
-        queue.task_done()
+            # # 保存数据
+            # savePath = os.path.join('土地数据', '地块公示_详情.csv')
+            # # 判断是否存在当前文件夹
+            # if not os.path.exists(savePath):  # 不存在
+            #     with open(savePath, 'a', newline='', encoding='gbk') as csvfile:
+            #         writer = csv.writer(csvfile)
+            #         lock = Lock()
+            #         lock.acquire()
+            #         writer.writerow(list(item.keys()))
+            #         writer.writerow(list(item.values()))
+            #         lock.release()
+            # else:
+            #     with open(savePath, 'a', newline='', encoding='gbk') as csvfile:
+            #         writer = csv.writer(csvfile)
+            #         lock = Lock()
+            #         lock.acquire()
+            #         writer.writerow(list(item.values()))
+            #         lock.release()
+        # print()
+        # queue.task_done()
 
 
 def run(threadNumber, df_map):
-    with open('土地log/地块公示坏url.text', 'r') as fp:
-        d = fp.readlines()
-    d = [i.strip() for i in d]
-
-    f = open(r'土地数据/地块公示.csv', encoding='gbk', mode='r')
-    df = pd.read_csv(f, error_bad_lines=False)
-    f.close()
-
-    df.rename(columns={'详情url':'标题url'}, inplace=True)
-    df = df[~df['行政区'].isin(['511603000', '511903000', '421303000','341504000', '440309000', '440310000', '330383000', '350598000'])]
-    df = df[~df['标题url'].isin(d)]
-
-    # 这里是筛选日期
-    # df['公示日期'] = df['公示日期'].map(lambda x: '' if '20' not in x or '公示' in x else x)
-    # df['公示日期'] = pd.to_datetime(df['公示日期'])
-    # df = df.query("公示日期 > '2020-06-01'")
-    # df['公示日期'] = df['公示日期'].map(lambda x: str(x).split(' ')[0])
-
-    print("df.shape: ", df.shape)
-    df = df.drop_duplicates(subset='标题url')
-    print("去重后: ", df.shape)
-    try:
-        f = open(r'土地数据/地块公示_详情.csv', encoding='gbk', mode='r', errors='ignore')
-        df1 = pd.read_csv(f, error_bad_lines=False)
-        df2 = df1.drop(index=df1[df1['标题url'].isnull()].index)
-        df2 = df2.drop(index=df2[df2['地块编号'].isnull()].index)
-        # df1.drop()
-        print('df1: ', df1.shape)
-        f.close()
-        df = df[~df['标题url'].isin(df2['标题url'].tolist())]
-    except Exception as e:
-        print(e)
-
-    print("去除已经解析后: ", df.shape)
-
-    number = df.shape[0]
-    for i in [df.iloc[i: i + 1] for i in range(number)]:
-        queue.put(i)
-
-    for i in range(threadNumber):
-        t = Thread(target=parse, args=(df_map,))
-        t.daemon = True
-        t.start()
-    time.sleep(3)
-    queue.join()
+    # with open('土地log/地块公示坏url.text', 'r') as fp:
+    #     d = fp.readlines()
+    # d = [i.strip() for i in d]
+    #
+    # f = open(r'土地数据/地块公示.csv', encoding='gbk', mode='r')
+    # df = pd.read_csv(f, error_bad_lines=False)
+    # f.close()
+    #
+    # df.rename(columns={'详情url':'标题url'}, inplace=True)
+    # df = df[~df['行政区'].isin(['511603000', '511903000', '421303000','341504000', '440309000', '440310000', '330383000', '350598000'])]
+    # df = df[~df['标题url'].isin(d)]
+    #
+    # # 这里是筛选日期
+    # # df['公示日期'] = df['公示日期'].map(lambda x: '' if '20' not in x or '公示' in x else x)
+    # # df['公示日期'] = pd.to_datetime(df['公示日期'])
+    # # df = df.query("公示日期 > '2020-06-01'")
+    # # df['公示日期'] = df['公示日期'].map(lambda x: str(x).split(' ')[0])
+    #
+    # print("df.shape: ", df.shape)
+    # df = df.drop_duplicates(subset='标题url')
+    # print("去重后: ", df.shape)
+    # try:
+    #     f = open(r'土地数据/地块公示_详情.txt', encoding='gbk', mode='r', errors='ignore')
+    #     df1 = pd.read_csv(f, error_bad_lines=False)
+    #     df2 = df1.drop(index=df1[df1['标题url'].isnull()].index)
+    #     df2 = df2.drop(index=df2[df2['地块编号'].isnull()].index)
+    #     # df1.drop()
+    #     print('df1: ', df1.shape)
+    #     f.close()
+    #     df = df[~df['标题url'].isin(df2['标题url'].tolist())]
+    # except Exception as e:
+    #     print(e)
+    #
+    # print("去除已经解析后: ", df.shape)
+    #
+    # number = df.shape[0]
+    # for i in [df.iloc[i: i + 1] for i in range(number)]:
+    #     queue.put(i)
+    parse(df_map)
+    # for i in range(threadNumber):
+    #     t = Thread(target=parse, args=(df_map,))
+    #     t.daemon = True
+    #     t.start()
+    # time.sleep(3)
+    # queue.join()
 
 
 if __name__ == '__main__':
     # df1: (288439, 16)
     # 去除已经解析后: (284652, 4) # 283754  274289
+    f = open('土地数据/地块公示.csv', encoding='gbk', mode='r')
     df_map = pd.read_excel('城市_区县_映射表.xlsx')
-    queue = Queue()
-    run(15, df_map)
+    # queue = Queue()
+    run(1, df_map)
+    f.close()
--- 土地市场网招拍挂/出让公告.py
+++ 土地市场网招拍挂/出让公告.py
@@ -9,8 +9,9 @@ import requests
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 from urllib.parse import quote
-from scrapy import Selector
+# from scrapy import Selector
 from config import delete_proxy
+from lxml import etree
 
 formDataMap = {
     'VIEWSTATE': '/wEPDwUJNjkzNzgyNTU4D2QWAmYPZBYIZg9kFgICAQ9kFgJmDxYCHgdWaXNpYmxlaGQCAQ9kFgICAQ8WAh4Fc3R5bGUFIEJBQ0tHUk9VTkQtQ09MT1I6I2YzZjVmNztDT0xPUjo7ZAICD2QWAgIBD2QWAmYPZBYCZg9kFgJmD2QWBGYPZBYCZg9kFgJmD2QWAmYPZBYCZg9kFgJmDxYEHwEFIENPTE9SOiNEM0QzRDM7QkFDS0dST1VORC1DT0xPUjo7HwBoFgJmD2QWAgIBD2QWAmYPDxYCHgRUZXh0ZWRkAgEPZBYCZg9kFgJmD2QWAmYPZBYEZg9kFgJmDxYEHwEFhwFDT0xPUjojRDNEM0QzO0JBQ0tHUk9VTkQtQ09MT1I6O0JBQ0tHUk9VTkQtSU1BR0U6dXJsKGh0dHA6Ly93d3cubGFuZGNoaW5hLmNvbS9Vc2VyL2RlZmF1bHQvVXBsb2FkL3N5c0ZyYW1lSW1nL3hfdGRzY3dfc3lfamhnZ18wMDAuZ2lmKTseBmhlaWdodAUBMxYCZg9kFgICAQ9kFgJmDw8WAh8CZWRkAgIPZBYCZg9kFgJmD2QWAmYPZBYCZg9kFgJmD2QWAmYPZBYEZg9kFgJmDxYEHwEFIENPTE9SOiNEM0QzRDM7QkFDS0dST1VORC1DT0xPUjo7HwBoFgJmD2QWAgIBD2QWAmYPDxYCHwJlZGQCAg9kFgJmD2QWBGYPZBYCZg9kFgJmD2QWAmYPZBYCZg9kFgJmD2QWAmYPFgQfAQUgQ09MT1I6I0QzRDNEMztCQUNLR1JPVU5ELUNPTE9SOjsfAGgWAmYPZBYCAgEPZBYCZg8PFgIfAmVkZAICD2QWBGYPZBYCZg9kFgJmD2QWAmYPZBYCAgEPZBYCZg8WBB8BBYwBQ09MT1I6I0QzRDNEMztCQUNLR1JPVU5ELUNPTE9SOjtCQUNLR1JPVU5ELUlNQUdFOnVybChodHRwOi8vd3d3LmxhbmRjaGluYS5jb20vVXNlci9kZWZhdWx0L1VwbG9hZC9zeXNGcmFtZUltZy94X3Rkc2N3X3p5X2NyZ2cyMDExTkhfMDEuZ2lmKTsfAwUCNDYWAmYPZBYCAgEPZBYCZg8PFgIfAmVkZAIBD2QWAmYPZBYCZg9kFgJmD2QWAgIBD2QWAmYPFgQfAQUgQ09MT1I6I0QzRDNEMztCQUNLR1JPVU5ELUNPTE9SOjsfAGgWAmYPZBYCAgEPZBYCZg8PFgIfAmVkZAIDD2QWAgIDDxYEHglpbm5lcmh0bWwFiQw8UD48QlI+PC9QPjxUQUJMRT48VEJPRFk+PFRSIGNsYXNzPWZpcnN0Um93PjxURCBzdHlsZT0iQk9SREVSLUJPVFRPTTogMXB4IHNvbGlkOyBCT1JERVItTEVGVDogMXB4IHNvbGlkOyBCT1JERVItVE9QOiAxcHggc29saWQ7IEJPUkRFUi1SSUdIVDogMXB4IHNvbGlkOyBib3JkZXI6MHB4IHNvbGlkIiB2QWxpZ249dG9wIHdpZHRoPTM3MD48UCBzdHlsZT0iVEVYVC1BTElHTjogY2VudGVyIj48QSBocmVmPSJodHRwczovL3d3dy5sYW5kY2hpbmEuY29tLyIgdGFyZ2V0PV9zZWxmPjxJTUcgdGl0bGU9dGRzY3dfbG9nZTEucG5nIGFsdD10ZHNjd19sb2dlMS5wbmcgc3JjPSJodHRwOi8vMjE4LjI0Ni4yMi4xNjYvbmV3bWFuYWdlL3VlZGl0b3IvdXRmOC1uZXQvbmV0L3VwbG9hZC9pbWFnZS8yMDIwMDYxMC82MzcyNzQwNjM0Mjg3NzExMDgxMTExMzEyLnBuZyI+PC9BPjwvUD48L1REPjxURCBzdHlsZT0iQk9SREVSLUJPVFRPTTogMXB4IHNvbGlkOyBCT1JERVItTEVGVDogMXB4IHNvbGlkOyBXT1JELUJSRUFLOiBicmVhay1hbGw7IEJPUkRFUi1UT1A6IDFweCBzb2xpZDsgQk9SREVSLVJJR0hUOiAxcHggc29saWQ7Ym9yZGVyOjBweCBzb2xpZCIgdkFsaWduPXRvcCB3aWR0aD02MjA+PFNQQU4gc3R5bGU9IkZPTlQtRkFNSUxZOiDlrovkvZMsIFNpbVN1bjsgQ09MT1I6IHJnYigyNTUsMjU1LDI1NSk7IEZPTlQtU0laRTogMTJweCI+5Li75Yqe77ya6Ieq54S26LWE5rqQ6YOo5LiN5Yqo5Lqn55m76K6w5Lit5b+D77yI6Ieq54S26LWE5rqQ6YOo5rOV5b6L5LqL5Yqh5Lit5b+D77yJPC9TUEFOPiA8UD48U1BBTiBzdHlsZT0iRk9OVC1GQU1JTFk6IOWui+S9kywgU2ltU3VuOyBDT0xPUjogcmdiKDI1NSwyNTUsMjU1KTsgRk9OVC1TSVpFOiAxMnB4Ij7mjIflr7zljZXkvY3vvJroh6rnhLbotYTmupDpg6joh6rnhLbotYTmupDlvIDlj5HliKnnlKjlj7gmbmJzcDsgJm5ic3A75oqA5pyv5pSv5oyB77ya5rWZ5rGf6Ie75ZaE56eR5oqA6IKh5Lu95pyJ6ZmQ5YWs5Y+4PC9TUEFOPiA8UD48U1BBTiBzdHlsZT0iRk9OVC1GQU1JTFk6IOWui+S9kywgU2ltU3VuOyBDT0xPUjogcmdiKDI1NSwyNTUsMjU1KTsgRk9OVC1TSVpFOiAxMnB4Ij7kuqxJQ1DlpIcxMjAzOTQxNOWPty00Jm5ic3A7ICZuYnNwO+S6rOWFrOe9keWuieWkhzExMDEwMjAwMDY2NigyKSZuYnNwOyAmbmJzcDvpgq7nrrHvvJpsYW5kY2hpbmEyMThAMTYzLmNvbSZuYnNwOyZuYnNwOzxzY3JpcHQgdHlwZT0idGV4dC9qYXZhc2NyaXB0Ij52YXIgX2JkaG1Qcm90b2NvbCA9ICgoImh0dHBzOiIgPT0gZG9jdW1lbnQubG9jYXRpb24ucHJvdG9jb2wpID8gIiBodHRwczovLyIgOiAiIGh0dHBzOi8vIik7ZG9jdW1lbnQud3JpdGUodW5lc2NhcGUoIiUzQ3NjcmlwdCBzcmM9JyIgKyBfYmRobVByb3RvY29sICsgImhtLmJhaWR1LmNvbS9oLmpzJTNGODM4NTM4NTljNzI0N2M1YjAzYjUyNzg5NDYyMmQzZmEnIHR5cGU9J3RleHQvamF2YXNjcmlwdCclM0UlM0Mvc2NyaXB0JTNFIikpOzwvc2NyaXB0PjwvU1BBTj4gPC9QPjwvVFI+PC9UQk9EWT48L1RBQkxFPjxQPiZuYnNwOzwvUD4fAQVkQkFDS0dST1VORC1JTUFHRTp1cmwoaHR0cDovL3d3dy5sYW5kY2hpbmEuY29tL1VzZXIvZGVmYXVsdC9VcGxvYWQvc3lzRnJhbWVJbWcveF90ZHNjdzIwMTNfeXdfMS5qcGcpO2Rkllk56OHtkasjpVJutwQ+oiEwAzq9DBYuOLwoA8WGSRA=',
@@ -134,7 +135,8 @@ def parse_page(page, **kwargs):
     if not res:
         print('没有res')
         return
-    response = Selector(text=res.text)
+    # response = Selector(text=res.text)
+    response = etree.HTML(res.text)
     d_list = []
 
     for tr in response.xpath('//*[@id="TAB_contentTable"]//tr[@id]'):
@@ -193,7 +195,8 @@ def run(startdate, enddate, df_map):
         time.sleep(random.randint(10, 15) * 0.1 * 3)
         res = get_html(QuerySubmitConditionData)
         if not res: continue
-        response = Selector(text=res.text)
+        # response = Selector(text=res.text)
+        response = etree.HTML(res.text)
 
         try:
             totalPage = "".join(
@@ -282,7 +285,7 @@ def run(startdate, enddate, df_map):
 
 if __name__ == '__main__':
     # startdate, enddate = '2011-1-1', '2020-11-1'
-    startdate, enddate = '2020-11-1', '2020-12-25'
+    startdate, enddate = '2021-1-1', '2021-4-19'
     df_map = pd.read_excel('城市_区县_映射表.xlsx')
     threadPool = ThreadPoolExecutor(max_workers=12)
     future = threadPool.submit(run ,startdate,enddate,df_map)
