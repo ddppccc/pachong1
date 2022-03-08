b78cc1698bf5d4e358dcf0a0a6e0d058befde46d
陈吉义 <chenjiyi@baojinhui.com>
2022-03-08 14:43:24 +0800

    bk_esf

---
+++ 房天下小区_二手房/config2.py
@@ -0,0 +1,17 @@
+import requests
+def get_proxy():
+    try:
+        return requests.get('http://1.116.204.248:5454/proxy2').text
+        # return requests.get('http://1.116.204.248:5000/proxy').text
+        # return requests.get('http://demo.spiderpy.cn/get/').json()['proxy']
+        # return requests.get('http://47.106.223.4:50002/get/').json()['proxy']
+    except:
+        num = 3
+        while num:
+            try:
+                return requests.get('http://demo.spiderpy.cn/get/').json()['proxy']
+            except:
+                print('暂无ip，等待20秒')
+                time.sleep(20)
+                num -= 1
+        print('暂无ip')
